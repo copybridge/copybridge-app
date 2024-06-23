68195baca245980c4c06ec84a90c333d2d935949
Abhishek M J <abhishekmj303@gmail.com>
2024-06-24 00:47:12 +0530

    Initial test clap - working

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,8 +2,22 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::process;
-use tauri::App;
-use tauri_plugin_cli::CliExt;
+
+use tauri::{AppHandle, Manager};
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: Option<String>,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 0)]
+    count: u8,
+}
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -11,62 +25,44 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+fn clap_cli() {
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name.unwrap_or_default());
+        process::exit(0);
+    }
+}
+
 fn main() {
-    let app = tauri::Builder::default()
-        .plugin(tauri_plugin_cli::init())
-        // .plugin(tauri_plugin_single_instance::init(|_app, _args, _cwd| {
-        //     // if args.len() < 2 {
-        //     //     let _ = show_window(app);
-        //     // }
-        // }))
-        .plugin(tauri_plugin_process::init())
+    clap_cli();
+
+    tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = show_window(app);
+        }))
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_shell::init())
-        // .setup(|app| {
-        //     let _ = handle_cli(app);
-        //     Ok(())
-        // })
         .invoke_handler(tauri::generate_handler![greet])
-        .build(tauri::generate_context!())
+        .run(tauri::generate_context!())
         .expect("error while running tauri application");
 
-    let _ = handle_cli(&app);
-
-    app.run(|_app_handle, event| match event {
-        // tauri::RunEvent::ExitRequested { api, .. } => {
-        //     api.prevent_exit();
-        // }
-        _ => {}
-    })
+    // app.run(|_app_handle, event| match event {
+    //     // tauri::RunEvent::ExitRequested { api, .. } => {
+    //     //     api.prevent_exit();
+    //     // }
+    //     _ => {}
+    // })
 
 }
 
-// fn show_window(app: &App) {
-//     let windows = app.webview_windows();
+fn show_window(app: &AppHandle) {
+    let windows = app.webview_windows();
 
-//     windows
-//         .values()
-//         .next()
-//         .expect("Sorry, no window found")
-//         .set_focus()
-//         .expect("Can't Bring Window to Focus");
-// }
-
-fn handle_cli(app: &App) {
-    match app.cli().matches() {
-        Ok(matches) => {
-            // println!("CLI matches: {:?}", matches);
-            // process::exit(0);
-
-            if matches.args.contains_key("help") {
-                print!(
-                    "{}",
-                    matches.args.get("help").unwrap().value.as_str().unwrap()
-                );
-                // app.cleanup_before_exit();
-                process::exit(0);
-            }
-        }
-        Err(_) => {}
-    }
+    windows
+        .values()
+        .next()
+        .expect("Sorry, no window found")
+        .set_focus()
+        .expect("Can't Bring Window to Focus");
 }
