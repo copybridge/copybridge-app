8c8275067c9cb2f958f3fd7bd11973b354751456
Siddesh Devarakonda <siddeshdslr@gmail.com>
2024-07-02 21:55:53 +0530

    Add router, create settings, home routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,61 @@
 import { invoke } from "@tauri-apps/api/core";
-import { 
-  AlertCircle,
-} from "lucide-react"
+import { AlertCircle } from "lucide-react";
 import {
   Alert,
   AlertDescription,
   AlertTitle,
-} from "@/components/ui/alert"
+} from "@/components/ui/alert";
 import ClipBoard from "./components/ClipBoard";
 import Navbar from "./components/Navbar";
-import { useEffect, useState } from "react"
+import Settings from "./components/Settings"; // Make sure to import Settings
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  let defaultConfig = {
+  const defaultConfig = {
     server: "",
     clipboards: []
   };
-  const [config, setConfig] = useState(defaultConfig)
+
+  const [config, setConfig] = useState(defaultConfig);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     invoke("read_config")
-        .then((res) => {
-            setConfig(res);
-            setError(null);
-            console.log("read config")
-        })
-        .catch((err) => {
-            setError(err);
-        });
-} , [])
+      .then((res) => {
+        setConfig(res);
+        setError(null);
+        console.log("read config");
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  }, []);
 
   return (
-    <>
+    <BrowserRouter>
       <Navbar config={config} setConfig={setConfig} setError={setError} />
       {error && (
         <Alert variant="destructive" className="text-red-400">
           <AlertCircle className="h-5 w-5" />
           <AlertTitle>Error</AlertTitle>
           <AlertDescription>
-          <span className="bold">{error.title}</span>: {error.message}
+            <span className="bold">{error.title}</span>: {error.message}
           </AlertDescription>
         </Alert>
       )}
-      <ClipBoard config={config} setConfig={setConfig} setError={setError} />
-    </>
-  )
+      <Routes>
+        <Route 
+          path="/" 
+          element={<ClipBoard config={config} setConfig={setConfig} setError={setError} />} 
+        />
+        <Route 
+          path="/settings" 
+          element={<Settings />} 
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
-import { invoke } from "@tauri-apps/api/core"
+import { invoke } from "@tauri-apps/api/core";
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Input } from "@/components/ui/input";
@@ -19,9 +20,10 @@ import {
   Plus,
   Loader2,
   Check,
-} from "lucide-react"
+  Settings
+} from "lucide-react";
 
-const Navbar = ({config, setConfig, setError}) => {
+const Navbar = ({ config, setConfig, setError }) => {
   const [showPasswordFieldCreate, setShowPasswordFieldCreate] = useState(false);
   const [showPasswordFieldAdd, setShowPasswordFieldAdd] = useState(false);
 
@@ -43,14 +45,14 @@ const Navbar = ({config, setConfig, setError}) => {
   const onCreate = () => {
     setNewClipboardState('adding');
     if (showPasswordFieldCreate && clipboardPassword === '') {
-      setError({title: "Failed to create clipboard", message: "Password is required"});
+      setError({ title: "Failed to create clipboard", message: "Password is required" });
       setNewClipboardState(null);
       return;
     }
     if (!showPasswordFieldCreate) {
       setClipboardPassword('');
     }
-    invoke("create", { config: config, args: { name: clipboardName, password: clipboardPassword, force: false} })
+    invoke("create", { config: config, args: { name: clipboardName, password: clipboardPassword, force: false } })
       .then((config) => {
         setError(null);
         setConfig(config);
@@ -64,20 +66,20 @@ const Navbar = ({config, setConfig, setError}) => {
       .catch((err) => {
         setError(err);
         setNewClipboardState(null);
-      })
-  }
+      });
+  };
 
   const onAdd = () => {
     setNewClipboardState('adding');
     if (showPasswordFieldAdd && addClipboardPassword === '') {
-      setError({title: "Failed to add clipboard", message: "Password is required"});
+      setError({ title: "Failed to add clipboard", message: "Password is required" });
       setNewClipboardState(null);
       return;
     }
     if (!showPasswordFieldAdd) {
       setAddClipboardPassword('');
     }
-    invoke("add", { config: config, args: { id: Number(addClipboardId), password: addClipboardPassword, force: false} })
+    invoke("add", { config: config, args: { id: Number(addClipboardId), password: addClipboardPassword, force: false } })
       .then((config) => {
         setError(null);
         setConfig(config);
@@ -91,118 +93,127 @@ const Navbar = ({config, setConfig, setError}) => {
       .catch((err) => {
         // if error is a string
         if (typeof err === 'string') {
-          setError({title: "Failed to add clipboard", message: err});
+          setError({ title: "Failed to add clipboard", message: err });
         } else {
           setError(err);
         }
         setNewClipboardState(null);
-      })
-  }
+      });
+  };
 
   return (
     <nav className='flex flex-row justify-between p-2'>
-      <p className='text-xl m-2'>CopyBridge</p>
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button
-            size="sm"
-            variant=""
-            className='order-last m-1'
-            disabled={newClipboardState === 'adding'}
-          >
-            {newClipboardState === 'adding' && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-            {newClipboardState === 'added' && <Check className="mr-2 h-4 w-4" />}
-            {!newClipboardState && <Plus className="mr-2 h-4 w-4" />}
-            New Clipboard
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="w-[400px]">
-          <Tabs defaultValue="create-clipboard" className="w-[350px]">
-            <TabsList>
-              <TabsTrigger value="create-clipboard">Create Clipboard</TabsTrigger>
-              <TabsTrigger value="add-clipboard">Add Clipboard</TabsTrigger>
-            </TabsList>
+      <Link to="/">
+        <p className='text-xl m-2'>CopyBridge</p>
+      </Link>
+      <div className='flex space-x-4'>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button
+              size="sm"
+              variant=""
+              className='m-1'
+              disabled={newClipboardState === 'adding'}
+            >
+              {newClipboardState === 'adding' && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+              {newClipboardState === 'added' && <Check className="mr-2 h-4 w-4" />}
+              {!newClipboardState && <Plus className="mr-2 h-4 w-4" />}
+              New Clipboard
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[400px]">
+            <Tabs defaultValue="create-clipboard" className="w-[350px]">
+              <TabsList>
+                <TabsTrigger value="create-clipboard">Create Clipboard</TabsTrigger>
+                <TabsTrigger value="add-clipboard">Add Clipboard</TabsTrigger>
+              </TabsList>
 
-            <TabsContent value="create-clipboard">
-              <div className="space-y-1">
-                <Label htmlFor="name">Clipboard Name</Label>
-                <Input
-                  id="name"
-                  value={clipboardName}
-                  onChange={(e) => setClipboardName(e.target.value)}
-                />
-              </div>
-              {showPasswordFieldCreate && (
+              <TabsContent value="create-clipboard">
                 <div className="space-y-1">
-                  <Label htmlFor="password">Password</Label>
-                  <PasswordInput
-                    id="password"
-                    value={clipboardPassword}
-                    onChange={(e) => setClipboardPassword(e.target.value)}
+                  <Label htmlFor="name">Clipboard Name</Label>
+                  <Input
+                    id="name"
+                    value={clipboardName}
+                    onChange={(e) => setClipboardName(e.target.value)}
                   />
                 </div>
-              )}
-              <div className="flex items-center mt-3.5 justify-between">
-                <div className='flex items-center space-x-2 mt-3.5'>
-                  <label
-                    htmlFor="encrypted"
-                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                  >
-                    Encrypt it
-                  </label>
-                  <Checkbox
-                    id="encrypted"
-                    onClick={openPasswordCreate}
-                    checked={showPasswordFieldCreate}
-                  />
+                {showPasswordFieldCreate && (
+                  <div className="space-y-1">
+                    <Label htmlFor="password">Password</Label>
+                    <PasswordInput
+                      id="password"
+                      value={clipboardPassword}
+                      onChange={(e) => setClipboardPassword(e.target.value)}
+                    />
+                  </div>
+                )}
+                <div className="flex items-center mt-3.5 justify-between">
+                  <div className='flex items-center space-x-2 mt-3.5'>
+                    <label
+                      htmlFor="encrypted"
+                      className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    >
+                      Encrypt it
+                    </label>
+                    <Checkbox
+                      id="encrypted"
+                      onClick={openPasswordCreate}
+                      checked={showPasswordFieldCreate}
+                    />
+                  </div>
+                  <DialogClose asChild>
+                    <Button size="sm" className='order-last m-1' onClick={onCreate}>Create</Button>
+                  </DialogClose>
                 </div>
-                <DialogClose asChild>
-                  <Button size="sm" className='order-last m-1' onClick={onCreate}>Create</Button>
-                </DialogClose>
-              </div>
-            </TabsContent>
-            <TabsContent value="add-clipboard">
-              <div className="space-y-1">
-                <Label htmlFor="clipboard-id">Clipboard ID</Label>
-                <Input
-                  id="clipboard-id"
-                  type="number"
-                  value={addClipboardId}
-                  onChange={(e) => setAddClipboardId(e.target.value)}
-                />
-              </div>
-              {showPasswordFieldAdd && (
+              </TabsContent>
+              <TabsContent value="add-clipboard">
                 <div className="space-y-1">
-                  <Label htmlFor="add-password">Password</Label>
-                  <PasswordInput
-                    id="add-password"
-                    value={addClipboardPassword}
-                    onChange={(e) => setAddClipboardPassword(e.target.value)}
+                  <Label htmlFor="clipboard-id">Clipboard ID</Label>
+                  <Input
+                    id="clipboard-id"
+                    type="number"
+                    value={addClipboardId}
+                    onChange={(e) => setAddClipboardId(e.target.value)}
                   />
                 </div>
-              )}
-              <div className="flex items-center mt-3.5 justify-between">
-                <div className='flex items-center space-x-2 mt-3.5'>
-                  <label
-                    htmlFor="encrypted-add"
-                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                  >
-                    Is Encrypted
-                  </label>
-                  <Checkbox
-                    id="encrypted-add"
-                    onClick={openPasswordAdd}
-                    checked={showPasswordFieldAdd}
-                  />
+                {showPasswordFieldAdd && (
+                  <div className="space-y-1">
+                    <Label htmlFor="add-password">Password</Label>
+                    <PasswordInput
+                      id="add-password"
+                      value={addClipboardPassword}
+                      onChange={(e) => setAddClipboardPassword(e.target.value)}
+                    />
+                  </div>
+                )}
+                <div className="flex items-center mt-3.5 justify-between">
+                  <div className='flex items-center space-x-2 mt-3.5'>
+                    <label
+                      htmlFor="encrypted-add"
+                      className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    >
+                      Is Encrypted
+                    </label>
+                    <Checkbox
+                      id="encrypted-add"
+                      onClick={openPasswordAdd}
+                      checked={showPasswordFieldAdd}
+                    />
+                  </div>
+                  <DialogClose asChild>
+                    <Button size="sm" className='order-last m-1' onClick={onAdd}>Add</Button>
+                  </DialogClose>
                 </div>
-                <DialogClose asChild>
-                  <Button size="sm" className='order-last m-1' onClick={onAdd}>Add</Button>
-                </DialogClose>
-              </div>
-            </TabsContent>
-          </Tabs>
-        </DialogContent>
-      </Dialog>
+              </TabsContent>
+            </Tabs>
+          </DialogContent>
+        </Dialog>
+        <Link to="/settings">
+          <Button size="sm" variant="ghost" className='m-1 order-last'>
+            <Settings className="" />
+          </Button>
+        </Link>
+      </div>
     </nav>
   );
 };
